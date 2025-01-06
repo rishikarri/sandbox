d372a070a3b5f4f14af4a5038c23894e7b098dd0
Rishi Karri <rishikarri1@gmail.com>
2025-01-06 12:40:24 -0500

    insertion sort works

--- insertionSort.js
+++ insertionSort.js
@@ -1,39 +1,22 @@
 
 // Take an unsorted array and return a new sorted array
-function insertionSort(arr) { 
-    const arrCopy = arr.slice(); // copy the array so we don't mutate the original arr
-
-    for (let i = 0; i < arrCopy.length; i++) {
-        let itemToSort = i + 1; 
-        let swapSpot = itemToSort;
-        for (let j = i; j >= 0; j--) {
-            if (arrCopy[itemToSort] < arrCopy[j]) {
-                // update swap spot
-                swapSpot = j;
-                
-
-                // const 
-                
-            }            
-        }
-
-        if (swapSpot !== itemToSort) {
-            const temp = arrCopy[swapSpot];
-            arrCopy[swapSpot] = arrCopy[itemToSort];
-            arrCopy[itemToSort] = arrCopy[swapSpot];
-            console.log("SWAP")
-        }
-        
-        // if (itemToSort )
-        
-        
-
-        
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let currentVal = arr[i];
+      let j = i - 1;
+  
+      while (j >= 0 && arr[j] > currentVal) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+  
+      arr[j + 1] = currentVal;
     }
-    return arrCopy;        
-}
+  
+    return arr;
+  }
 
 // test results
-const unsortedArr = [1, 2, 17, 3, 99, 6]
+const unsortedArr = [4, 2, 5, 17, 3, 99, 6]
 const sortedArr = insertionSort(unsortedArr)
 console.log('sortedList', sortedArr)
