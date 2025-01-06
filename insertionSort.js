de5b03557dddeb679fa6363d5be537b4c044767b
Rishi Karri <rishikarri1@gmail.com>
2025-01-06 12:55:30 -0500

    fixed insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -2,21 +2,26 @@
 // Take an unsorted array and return a new sorted array
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-      let currentVal = arr[i];
-      let j = i - 1;
-  
-      while (j >= 0 && arr[j] > currentVal) {
-        arr[j + 1] = arr[j];
-        j--;
-      }
-  
-      arr[j + 1] = currentVal;
+        const currentVal = arr[i];
+
+        let j = i -1; 
+        while (j >=0 && arr[j] > currentVal) {
+            // move arr[j] one to the right by overwriting what is there             
+            arr[j + 1] = arr[j]            
+            j--;
+            
+        }
+
+
+        // write current value to most left spot 
+        // we can do this because the while loop stopped 
+        // this means that our current value is smaller than what's left of it
+        arr[j + 1] = currentVal
     }
-  
-    return arr;
+    return arr; 
   }
 
 // test results
-const unsortedArr = [4, 2, 5, 17, 3, 99, 6]
+const unsortedArr = [1, 2, 5, 17, 3, 99, 6]
 const sortedArr = insertionSort(unsortedArr)
 console.log('sortedList', sortedArr)
