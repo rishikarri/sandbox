32b6555907117c21e35c989b1f7c9fe41388c245
Rishi Karri <rishikarri1@gmail.com>
2025-01-14 10:26:59 -0500

    feat: merge sort works

---
+++ bigONotation.js
@@ -0,0 +1,41 @@
+// Takes an array of numbers 
+// and returns the sum of the last 2 numbers in the array
+function sumLast2(arr) {
+    const num1 = arr[arr.length - 1]
+    const num2 = arr[arr.length - 2]
+
+    const sum = num1 + num2; 
+
+    return sum; 
+}
+
+function sumLast2LessEfficient(arr) {
+    let num1;
+    let num2; 
+    for (let i = 0; i < arr.length; i++) {
+        if (i === arr.length - 1) { // come across the last item 
+            num1 = arr[i]
+        }
+
+        if (i === arr.length - 2) { // come across the second to last item 
+            num2 = arr[i]
+        }
+        
+    }
+
+    const sum = num1 + num2; 
+
+    return sum; 
+}
+
+
+
+
+
+
+const arrToSum = [1, 2, 6, 19, 2222, 12, 28, 29, 1, 3, 9, 1, 3, 4] // should return 7 
+
+const result = sumLast2(arrToSum);
+
+console.log(result);
+
--- mergeSort.js
+++ mergeSort.js
@@ -1,33 +1,48 @@
 // takes 2 sorted arrays and merges them into 1 larger sorted array
 function merge(arr, arr2) {
+	const newArr = [];
+	let i = 0;
+	let j = 0;
+	while (i < arr.length && j < arr2.length) {
+		if (arr[i] < arr2[j]) {
+			newArr.push(arr[i]);
+			i++;
+		}
+		if (arr2[j] < arr[i]) {
+			newArr.push(arr2[j]);
+			j++;
+		}
+	}
 
-    const newArr = [];
-    let i = 0; 
-    let j = 0; 
-    while (i < arr.length && j < arr2.length) {
-        if (arr[i] < arr2[j]) {
-            newArr.push(arr[i])
-            i++; 
-            
-        } 
-        if (arr2[j] < arr[i]) {
-            newArr.push(arr2[j])
-            j++;
-        }
-    }
+	while (i < arr.length) {
+		newArr.push(arr[i]);
+		i++;
+	}
 
-    while (i < arr.length) {
-        newArr.push(arr[i])
-        i++;
-    }
-
-    while (j < arr2.length) {
-        newArr.push(arr2[j])
-        j++;
-    }
-    return newArr;
+	while (j < arr2.length) {
+		newArr.push(arr2[j]);
+		j++;
+	}
+	return newArr;
 }
 
 
-console.log('MERGED', merge([1,3,4,99], [0, 5, 19, 29]))
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    }
+    let mid = Math.floor(arr.length / 2);
+	let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+	return merge(left, right);
+}
 
+// Example usage:
+const testArr = [2, 1289, 1, 44, 912, 129, 11, 9];
+const sortedArr = mergeSort(testArr);
+console.log(sortedArr);
+console.log('hi')
+// Output: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+// breakIntoArrsOf1([2, 1289, 1, 44, 12, 390, 1]);
+// console.log("MERGED", merge([1, 3, 4, 99], [0, 5, 19, 29]));
