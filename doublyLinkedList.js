92ec53bce47c4eb63e0ed34164b98d02089cd964
Rishi Karri <rishikarri1@gmail.com>
2025-01-16 15:41:33 -0500

    feat: update doubly linked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -26,4 +26,35 @@ class DoublyLinkedList {
 
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head 
+        this.length--;
+
+        let poppedNode;
+
+        if (this.length === 0) {
+            poppedNode = this.head
+            this.head = null;
+            this.tail = null
+            return poppedNode;
+        }
+
+        // traverse to the second last node 
+        
+
+        poppedNode = this.tail;
+        this.tail.previous = null; 
+        this.tail = this.tail.previous;
+        this.tail.next = null;
+        
+        return poppedNode;
+    }
 }
+
+const test1 = new DoublyLinkedList(); 
+
+test1.push("Hi");
+test1.push("Hello")
+test1.push(9999)
+
