d891bba9d8f19981203fc1e5c29a8b497fe94254
Rishi Karri <rishikarri1@gmail.com>
2025-02-28 12:42:59 -0500

    feat: remove edge func

--- data-structures-learning/graph.js
+++ data-structures-learning/graph.js
@@ -12,13 +12,23 @@ class Graph {
         this.adjacenyList[vertex1].push(vertex2)
         this.adjacenyList[vertex2].push(vertex1)
     }
+    removeEdge(vertex1, vertex2) {
+        this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter((v) => v !== vertex2)
+        this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter((v) => v !== vertex1)
+    }
 }
 
 a = new Graph()
 
 a.addVertex("Tokyo")
 a.addVertex("Dallas")
+a.addVertex("Atlanta")
+a.addVertex("Milwaukee")
 
 a.addEdge("Tokyo", "Dallas")
+a.addEdge("Atlanta", "Milwaukee")
+a.addEdge("Atlanta", "Tokyo")
+
+a.removeEdge("Atlanta", "Tokyo")
 
 console.log("HI")
