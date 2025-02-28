09d272a60b7417a3cda71225890cfdb4bf6c891d
Rishi Karri <rishikarri1@gmail.com>
2025-02-28 12:22:19 -0500

    feat: graph data structure simple implementation

---
+++ data-structures-learning/graph.js
@@ -0,0 +1,21 @@
+class Graph {
+    constructor() {
+        this.adjacenyList = {
+
+        } 
+    }
+    addVertex(vertex) {
+        this.adjacenyList[vertex] = []       
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacenyList[vertex1].push(vertex2)
+        this.adjacenyList[vertex2].push(vertex1)
+    }
+}
+
+a = new Graph()
+
+a.addVertex("Tokyo")
+
+console.log("HI")
