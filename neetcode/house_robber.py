1e6ec775c1f393c6f8bab662cf7a6de84770abce
Rishi Karri <rishikarri1@gmail.com>
2025-03-27 12:10:07 -0400

    feat: dp solution

--- neetcode/house_robber.py
+++ neetcode/house_robber.py
@@ -1,16 +1,21 @@
-from typing import List
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
-    
-nums=[1,1,3,3]
+        if len(nums) == 0: 
+            return 0
+        
+        
 
-result = Solution().rob(nums)
+        dp = [0] * len(nums)
 
-print(result, 'result')
+
+        for i in range(len(nums)):
+            if i == 0:
+                dp[i] = nums[0]
+            
+            if i == 1:
+                dp[i] = max(nums[0], nums[1])
+            if i >= 2:
+                dp[i] = max(nums[i] + dp[i - 2], dp[i-1])
+        
+        print(dp)
+        return dp[len(nums) - 1] 
